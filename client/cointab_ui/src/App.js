1d0c197ab8baa6be4fbb21cbec17595fea25b168
kothadiyasagar <skothadiya@harvices.com>
2022-12-06 20:09:55 +0530

    add login page

---
+++ client/cointab_ui/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './page/login';
+
+function App() {
+  return (
+    <div className="">
+     <Login />
+    </div>
+  );
+}
+
+export default App;
